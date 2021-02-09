7408a1deb94619431db364eb74025a82f50c9c55
prodigysurge <boatinglife6@yahoo.com>
2021-02-09 07:44:03 -0500

    commit tree 2ee785f35c868743070f5b74c228cf559eb9962a

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Cholcolate", 3, "M", "Brown", "Lazy", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 2, "F", "Black", "Mean", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 9, "F", "Brown", "Aggressive", 1);
+INSERT INTO bears VALUES (4, "Sergeant", 12, "M", "Polar", "Fiesty", 1);
+INSERT INTO bears VALUES (5, "Melissa", 4 , "F", "Grizzly", "Aggressive", 1);
+INSERT INTO bears VALUES (6, "Grinch", 8, "F", "Black", "Lazy", 1);
+INSERT INTO bears VALUES (7, "Wendy", 20, "M", "Brown", "Fiesty", 1);
+INSERT INTO bears VALUES (8, NULL, 12, "M", "Polar", "Aggressive", 1);
